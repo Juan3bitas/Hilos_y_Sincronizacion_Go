3414ca2ebc3d0301ec20b1005550af73e9990ea9
Juans3bitas <134520422+Juan3bitas@users.noreply.github.com>
2025-04-18 00:57:51 -0500

    Add files via upload

---
+++ hilos_y_sincronizacion_go/go.mod
@@ -0,0 +1,3 @@
+module hilos_y_sincronizacion_go
+
+go 1.24.2
---
+++ hilos_y_sincronizacion_go/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Contantes de máximo de usuarios (Empleados) que pueden usar la impresora a la vez y el semáforo
+const maxUsuarios = 1
+
+var semaforo = make(chan struct{}, maxUsuarios)
+
+// Estructura de Empleado
+type Empleado struct {
+	Nombre    string
+	Documento string
+}
+
+// Estructura de Impresora
+type Impresora struct {
+	Empleados []Empleado
+}
+
+func (i *Impresora) imprimir(empleado Empleado) {
+	//Solicitar acceso a la impresora
+	semaforo <- struct{}{}
+	fmt.Printf("\n%s está imprimiendo %s.\n", empleado.Nombre, empleado.Documento)
+	time.Sleep(5 * time.Second)
+	fmt.Printf("%s terminó de imprimir su documento.\n", empleado.Nombre)
+	i.removerEmpleado(empleado)
+	<-semaforo
+}
+
+func (i *Impresora) agregarEmpleado(empleado Empleado) {
+	i.Empleados = append(i.Empleados, empleado)
+}
+
+func (i *Impresora) removerEmpleado(empleado Empleado) {
+	for index, emp := range i.Empleados {
+		if emp.Nombre == empleado.Nombre {
+			i.Empleados = append(i.Empleados[:index], i.Empleados[index+1:]...)
+			break
+		}
+	}
+}
+
+func (i *Impresora) gestionarImpresiones() {
+	var wg sync.WaitGroup
+	for _, empleado := range i.Empleados {
+		wg.Add(1)
+		go func(emp Empleado) {
+			defer wg.Done()
+			i.imprimir(emp)
+		}(empleado)
+	}
+	wg.Wait()
+	fmt.Println("\nTodos los empleados ya imprimieron sus documentos.")
+}
+
+func main() {
+	//Se crea la impresora
+	impresora := Impresora{}
+
+	//Se agregan los usuarios que van a imprimir sus documentos a la lista de la impresora
+	impresora.agregarEmpleado(Empleado{"Angie Bonilla", "Tralalero_tralala.png"})
+	impresora.agregarEmpleado(Empleado{"Celeste Buitrago", "Albaricoque.docx"})
+	impresora.agregarEmpleado(Empleado{"Juan Diego", "Acta_de_matrimonio.pdf"})
+
+	//Se gestionan las peticiones de los empleados
+	impresora.gestionarImpresiones()
+
+	impresora.agregarEmpleado(Empleado{"Angie Grajales", "Allosaurus.png"})
+	impresora.agregarEmpleado(Empleado{"David Gómez", "Brawlhalla_stats.png"})
+	impresora.agregarEmpleado(Empleado{"Juan Sebastián", "Principe_gris_zote.pdf"})
+
+	impresora.gestionarImpresiones()
+}
